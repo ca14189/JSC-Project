895d0c9bd699148af7c07232559a95f810916fdf
pradip.jschamp <pradip.jschamp@gmail.com>
2026-02-13 15:58:00 +0530

    added meta data for job and blogs page

--- src/app/jobs/[slug]/page.tsx
+++ src/app/jobs/[slug]/page.tsx
@@ -1,4 +1,45 @@
 import ReactMarkdown from "react-markdown";
+import { Metadata } from "next";
+
+async function fetchMeta() {
+  const domain = "jschamps.com";
+  const group = "Job"; 
+  // const API_URL = "http://localhost:3001/api/cms/meta";
+
+ const url = `${process.env.NEXT_PUBLIC_BASE_URL}/meta?domain=${domain}&group=${group}`;
+
+  const res = await fetch(url, { cache: "no-store" });
+
+  if (!res.ok) return {};
+
+  const json = await res.json();
+  return json.data || {};
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const meta = await fetchMeta();
+  const content = meta.content ?? {};
+
+  return {
+    title: content.title ?? "Default Title",
+    description: content.description ?? "Default Description",
+
+    openGraph: content.openGraph,
+    twitter: content.twitter,
+    keywords: content.keywords,
+    robots: content.robots,
+    icons: content.icons,
+    authors: content.authors,
+
+    creator: content.creator,
+    publisher: content.publisher,
+    applicationName: content.applicationName,
+
+    metadataBase: content.metadataBase
+      ? new URL(content.metadataBase)
+      : undefined,
+  };
+}
 
 /* ================= FETCH JOB ================= */
 
