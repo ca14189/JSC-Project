565271a0f6593486463e4b116fc4df58be1ef74a
pradip.jschamp <pradip.jschamp@gmail.com>
2026-02-14 14:55:46 +0530

    changes in blogs ui

--- src/components/blogs/BlogCard.tsx
+++ src/components/blogs/BlogCard.tsx
@@ -12,17 +12,17 @@ export default function BlogCard({ blog }: Props) {
 
   return (
     <Link href={`/blogs/${blog.slug}`} className="block">
-      <article className="group relative bg-white/70 backdrop-blur-xl border border-gray-200 rounded-2xl overflow-hidden shadow-sm hover:shadow-xl transition-all duration-300 min-h-[470px]">
+      <article className="group relative bg-white/70 backdrop-blur-xl border border-gray-200 rounded-2xl overflow-hidden shadow-sm hover:shadow-xl transition-all duration-300 min-h-[520px] w-80">
         
         {/* Image */}
         <div className="relative h-56 overflow-hidden">
           <Image
-  src={blog?.coverImage || "/placeholder.jpg"}
+            src={blog?.coverImage || "/placeholder.jpg"}
 
-  alt={blog.title}
-  fill
-  className="object-cover transition-transform duration-500 group-hover:scale-110"
-/>
+            alt={blog.title}
+            fill
+            className="object-cover transition-transform duration-500 group-hover:scale-110"
+          />
 
           <span className="absolute top-4 left-4 text-xs font-semibold px-3 py-1 rounded-full bg-black text-white shadow">
             {blog.category}
@@ -32,12 +32,12 @@ export default function BlogCard({ blog }: Props) {
         {/* Content */}
         <div className="p-6">
           <p className="text-sm text-gray-500 mb-2">
-  {new Date(blog.date).toLocaleDateString('en-IN', {
-    day: '2-digit',
-    month: 'short',
-    year: 'numeric',
-  })}
-</p>
+            {new Date(blog.date).toLocaleDateString('en-IN', {
+              day: '2-digit',
+              month: 'short',
+              year: 'numeric',
+            })}
+          </p>
 
 
           <h3 className="text-lg font-semibold text-gray-900 leading-snug mb-3 group-hover:text-blue-800 transition">
